ce0ff5ce2d35f77e145b4c6208746d32bcb281ff
Nikolas Freitas <NikoalsFreitas@github.com>
2017-08-04 16:53:16 -0300

    Adjusted general layout

--- future/header.php
+++ future/header.php
@@ -3,30 +3,41 @@
 
 
 
-
-  <div class="mdl-cell mdl-cell--12-col">
-      <div class="mdl-layout__header-row">
-          <div class="mdl-layout-spacer"></div>
-          <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right">
-              <nav class="mdl-navigation grey-nav">
-                    <a class="mdl-navigation__link" href="">Acessibilidade</a>
-                    <a class="mdl-navigation__link" href="">Notificação</a>
-                    <a class="mdl-navigation__link" href="">Sair</a>
-              </nav>
+  <div class="content-grid mdl-grid top-shadow inset-shadow">
+      <div class="mdl-cell mdl-cell--12-col">
+          <div class="mdl-layout__header-row">
+              <div class="mdl-layout-spacer"></div>
+              <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right">
+                  <nav class="mdl-navigation grey-nav">
+                      <a class="mdl-navigation__link" href="">Acessibilidade</a>
+                      <a class="mdl-navigation__link" href="">Notificação</a>
+                      <a class="mdl-navigation__link" href="">Sair</a>
+                  </nav>
+              </div>
           </div>
       </div>
   </div>
 <header class="mdl-layout__header">
     <div class="mdl-layout__header-row">
         <div class="mdl-layout-spacer"></div>
-        <div class="mdl-textfield mdl-js-textfield mdl-textfield--expandable mdl-textfield--floating-label mdl-textfield--align-right">
-            <label class="mdl-button mdl-js-button mdl-button--icon"
-                   for="fixed-header-drawer-exp">
-              <i class="material-icons">search</i>
-            </label>
-            <div class="mdl-textfield__expandable-holder">
-              <input class="mdl-textfield__input" type="text" name="sample" id="fixed-header-drawer-exp">
+        <div class="content-grid mdl-grid">
+            <div class="mdl-cell mdl-cell--4-col">
+                <p>THINGS</p>
+            </div>
+            <div class="mdl-cell mdl-cell--8-col mdl-textfield--align-right">
+                <div class="mdl-textfield mdl-js-textfield">
+                    <p>THING</p>
+                </div>
             </div>
         </div>
     </div>
 </header>
+<div class="mdl-layout__drawer drawer-space top-shadow inset-shadow">
+  <span class="mdl-layout-title">Title</span>
+  <nav class="mdl-navigation">
+    <a class="mdl-navigation__link" href="">Link</a>
+    <a class="mdl-navigation__link" href="">Link</a>
+    <a class="mdl-navigation__link" href="">Link</a>
+    <a class="mdl-navigation__link" href="">Link</a>
+  </nav>
+</div>
