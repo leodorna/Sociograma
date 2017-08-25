59643276cb0c249b930a164a37253be8de1fe728
Nikolas Freitas <NikoalsFreitas@github.com>
2017-08-25 15:58:53 -0300

    Adjusted effects

--- js/front.js
+++ js/front.js
@@ -2,12 +2,18 @@ $(document).ready(function(){
     $('.map-select').click(function(){
         $('.map-select').removeClass('active');
         $(this).addClass('active');
+
+        if ($(this).attr('id') == 'mapCat') {
+            $('#Selecao').addClass('active');
+        } else {
+            $('#Selecao').removeClass('active');
+        }
     });
 
     $('#config-drawer').click(function(){
         $('.right-drawer').removeClass('not-visible');
         $('.right-drawer').removeClass('is-visible');
-        
+
         $('.mdl-layout__obfuscator').addClass('is-visible');
     });
 
